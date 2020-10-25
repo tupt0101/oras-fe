31530d1666b269922513e5ef5444bb3082c2ee66
leplep190599 <59325529+leplep190599@users.noreply.github.com>
2020-10-25 11:54:59 +0700

    fix cors for user api

--- src/api/user.js
+++ src/api/user.js
@@ -1,12 +1,14 @@
 import request from '@/utils/request'
 import axios from 'axios'
 
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
 // create an axios instance
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // baseURL: 'https://oras-api.herokuapp.com',
-  // baseURL: 'http://localhost:8080/oras',
-  baseURL: 'http://localhost/oras',
+  // baseURL: 'http://localhost:8088',
+  baseURL: 'http://localhost:9527/',
+  // baseURL: 'http://localhost/oras',
   withCredentials: true, // send cookies when cross-domain requests
   // credentials: 'same-origin',
   timeout: 30000 // request timeout
@@ -14,21 +16,23 @@ const service = axios.create({
 
 const headers = {
   'Authorization': 'Basic bXktdHJ1c3RlZC1jbGllbnQ6c2VjcmV0',
-  'Content-Type': 'multipart/form-data'
+  // 'Content-Type': 'multipart/form-data',
+  'Content-Type': 'application/json'
+  // 'Access-Control-Allow-Origin': '*'
 }
 
 export function login(data) {
-  const bodyFormData = new FormData()
-  bodyFormData.append('grant_type', 'password')
-  bodyFormData.append('username', data.username)
-  bodyFormData.append('password', data.password)
+  // const bodyFormData = new FormData()
+  // bodyFormData.append('grant_type', 'password')
+  // bodyFormData.append('username', data.username)
+  // bodyFormData.append('password', data.password)
   return service.request({
     // url: '/test-api/user/login',
     // url: '/v1/account-management/accounts',
-    url: '/login',
-    method: 'post',
-    headers: headers,
-    data: bodyFormData
+    url: '/login?username=' + data.username + '&password=' + data.password,
+    method: 'get',
+    headers: headers
+    // data: bodyFormData
   })
 }
 
