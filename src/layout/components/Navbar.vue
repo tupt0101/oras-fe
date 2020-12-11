8713b0da90bd17c48d3ffe148b61e99253bfc79a
Phan Thanh Tu <tupt0101@gmail.com>
2020-12-11 15:57:46 +0700

    edit navbar

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -28,20 +28,23 @@
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
-          <router-link to="/">
+          <!-- <router-link to="/">
             <el-dropdown-item>Dashboard</el-dropdown-item>
-          </router-link>
+          </router-link> -->
           <router-link to="/profile/index">
-            <el-dropdown-item>Profile</el-dropdown-item>
+            <el-dropdown-item>Welcome, {{ accountName }}<br>View Profile</el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/tupt0101/oras-fe">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <!-- <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a> -->
+          <el-dropdown-item divided>
+            <strong style="font-size: 1.1em" class="disabled no-hover">Manage</strong>
+          </el-dropdown-item>
+          <router-link to="/job/list">
+            <el-dropdown-item>Job List</el-dropdown-item>
+          </router-link>
+          <router-link to="/report/index">
+            <el-dropdown-item>Recruitment Report</el-dropdown-item>
+          </router-link>
           <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+            <span style="display:block;">Sign out</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
@@ -71,12 +74,20 @@ export default {
     // Notification,
     LangSelect
   },
+  data() {
+    return {
+      account: null
+    }
+  },
   computed: {
     ...mapGetters([
       'sidebar',
       'avatar',
       'device'
-    ])
+    ]),
+    accountName() {
+      return this.$store.state.user.name
+    }
   },
   methods: {
     toggleSideBar() {
@@ -92,6 +103,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.disabled {
+  pointer-events:none; //This makes it not clickable
+  opacity:0.6;         //This grays it out to look disabled
+}
+
+.no-hover:hover {
+  background-color: transparent;
+}
 .navbar {
   height: 50px;
   overflow: hidden;
